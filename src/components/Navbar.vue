4ece69dfff98dead8ba276dd2e359ffc93d11446
JuanDriko <juandriko@hotmail.com>
2024-01-20 02:35:52 -0500

    first commit moviesplay finish

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="flex mt-3">
-        <router-link to="/">
+        <router-link to="/MoviePlay/">
         <h1 class="flex m-5"><i class="fas fa-video"></i><strong class="ml-2"> MoviePlay</strong></h1>
         </router-link>
         <ul class="flex mt-5">
 
-            <li><router-link to="/" class="ml-7">Inicio</router-link></li>
+            <li><router-link to="/MoviePlay/" class="ml-7">Inicio</router-link></li>
             <li><router-link to="/Genres/" class="ml-7">Géneros</router-link></li>
             
         </ul>
