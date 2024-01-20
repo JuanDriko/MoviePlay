51b4c81c785671bc6fb5a33486d00fbe9d6fb752
JuanDriko <juandriko@hotmail.com>
2024-01-20 03:02:23 -0500

    first commit moviesplay finish

--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/Index'
-import AppLink from "@/components/AppLink.vue";
-import '@fortawesome/fontawesome-free/css/all.css'
 import '../public/main.css'
 
 createApp(App)
-  .component('AppLink', AppLink)
   .use(router)
   .mount('#app')
